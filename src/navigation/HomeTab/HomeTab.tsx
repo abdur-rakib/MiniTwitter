554758d568b744367e36b4317a4edca857f6a03b
Abdur Rakib <abdurrakib961@gmail.com>
2023-04-06 12:35:09 +0600

    fix: Fix tab bar navigation styling

--- src/navigation/HomeTab/HomeTab.tsx
+++ src/navigation/HomeTab/HomeTab.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../../screens/Home';
 import SearchScreen from '../../screens/Search';
 import DummyScreen from '../../screens/Dummy';
+import {ScaledSheet} from 'react-native-size-matters';
+import {colors} from '../../theme/colors';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,14 +15,92 @@ const HomeTab = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarShowLabel: false,
+        headerShadowVisible: false,
+        tabBarActiveTintColor: colors.black,
+        tabBarInactiveTintColor: colors.black,
       }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="News" component={DummyScreen} />
-      <Tab.Screen name="Notifications" component={DummyScreen} />
-      <Tab.Screen name="Message" component={DummyScreen} />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <Ionicons
+              name={focused ? 'home' : 'home-outline'}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="Home"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <Ionicons
+              name={focused ? 'md-search' : 'search-outline'}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="Search"
+        component={SearchScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <Ionicons
+              name={focused ? 'ios-mic-sharp' : 'ios-mic-outline'}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="News"
+        component={DummyScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <Ionicons
+              name={
+                focused
+                  ? 'ios-notifications-sharp'
+                  : 'ios-notifications-outline'
+              }
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="Notifications"
+        component={DummyScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused, color, size}) => (
+            <Ionicons
+              name={focused ? 'ios-mail' : 'mail-outline'}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="Message"
+        component={DummyScreen}
+      />
     </Tab.Navigator>
   );
 };
+
+const styles = ScaledSheet.create({
+  tabBarStyle: {
+    backgroundColor: colors.extra_extra_light_gray,
+    borderTopWidth: 0.5,
+    borderTopColor: colors.extra_light_gray,
+    elevation: 0,
+  },
+});
 
 export default HomeTab;
